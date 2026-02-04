88019d3c5f100f16a074db8ec5d67b76ecf76c84
6652410026ThayakornKoomphai <s6652410026@sau.ac.th>
2026-02-04 16:05:15 +0700

    first commit

---
+++ app/_layout.tsx
@@ -0,0 +1,54 @@
+import {
+  Kanit_400Regular,
+  Kanit_700Bold,
+  useFonts,
+} from "@expo-google-fonts/kanit";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  //-----------------------------------------
+  const [fontsLoaded] = useFonts({
+    Kanit_400Regular,
+    Kanit_700Bold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+  if (!fontsLoaded) {
+    return null;
+  }
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="home"
+        options={{
+          title: "Top 10 Bangkok Coffee",
+          headerTitleAlign: "center",
+          headerTitleStyle: { fontFamily: "Kanit_400Regular", color: "#fff" },
+          headerStyle: { backgroundColor: "#714503" },
+        }}
+      />
+      <Stack.Screen
+        name="detail"
+        options={{
+          title: "รายละเอียดร้าน",
+          headerBackButtonDisplayMode: "minimal",
+          headerTitleAlign: "center",
+          headerTitleStyle: {
+            fontFamily: "Kanit_400Regular",
+            color: "#fff",
+          },
+          headerStyle: { backgroundColor: "#714503" },
+        }}
+      />
+    </Stack>
+  );
+}
